84df8c84620322ad59fdfb0f922de319a23050ed
Joseph Peyton <josephpeyton@joseph.local>
2014-02-06 10:53:19 -0500

    Facebook js

---
+++ js/fb.js
@@ -0,0 +1,36 @@
+<script>
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '{your-app-id}',
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+
+    FB.Event.subscribe('auth.authResponseChange', function(response) {
+    if (response.status === 'connected') {
+          testAPI();
+    } else if (response.status === 'not_authorized') {
+      FB.login();
+    } else {
+      FB.login();
+    }
+  });
+  };
+
+  // Load the SDK asynchronously
+  (function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+  }(document));
+
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Good to see you, ' + response.name + '.');
+    });
+  }
+</script>
