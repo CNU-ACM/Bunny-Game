40ad8d01d6434391bb8d923405eb8da21405e536
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2014-02-06 14:12:07 -0500

    Final Layer Bug Fix
    -Finally fixed the layer bug.

--- game/Characters/baseChar.js
+++ game/Characters/baseChar.js
@@ -1,10 +1,8 @@
 function loadCharacter(playerName, characterID, x, y) {
-    Lib().load(function () {
-        switch (characterID) {
-            case 0:
-                loadTestChar(playerName, x, y);
-                break;
-        }
-    });
+    switch (characterID) {
+        case 0:
+            loadTestChar(playerName, x, y);
+            break;
+    }
 }
 
--- game/Worlds/baseWorld.js
+++ game/Worlds/baseWorld.js
@@ -1,10 +1,8 @@
 function loadWorld(playerName, worldSpot, worldID, x, y) {
-    Lib().load(function () {
-        switch (worldID) {
-            case 0:
-                loadTestWorld(playerName, worldSpot, x, y);
-                break;
-        }
-    });
+    switch (worldID) {
+        case 0:
+            loadTestWorld(playerName, worldSpot, x, y);
+            break;
+    }
 }
 
