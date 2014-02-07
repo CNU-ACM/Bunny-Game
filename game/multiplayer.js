d3c29c1cbb2b6066e8e24593a41cc9a8419cdaad
juanvallejo <juan.vallejo.12@cnu.edu>
2014-02-07 01:32:39 -0500

    edited multiplayer.js

---
+++ game/multiplayer.js
@@ -0,0 +1,14 @@
+var socket = io.connect('http://10.67.31.165:8888');
+var charManager = {
+	mainChar:{
+		spawned:false
+	}
+};
+
+socket.on('welcome',function(res) {
+	console.log(res);
+});
+
+socket.on('playerData',function(data) {
+	console.log("A PLAYER HAS FUCKING CONNECTED!");
+});
