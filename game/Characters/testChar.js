f6e1bae2c087264711ea8303b49ab9516eca057e
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2014-02-06 20:30:09 -0500

    New Graphics
    -Updated with Bun graphics and tiles.

--- game/Characters/testChar.js
+++ game/Characters/testChar.js
@@ -1,10 +1,10 @@
 function loadTestChar(playerName, worldArray, x, y) {
     Lib("testChar").sprite({
-        src:"Game/Graphics/Characters/testChar.png",
-        size:[50,100],
-        position:[0,200],
+        src:"Game/Graphics/Characters/bun.png",
+        size:[712,712],
+        position:[0, 0],
         speed:70,
-        // scale:0.5,
+        scale:0.125,
         frequency: 13,
         x:x,
         y:y
@@ -17,36 +17,40 @@ function loadTestChar(playerName, worldArray, x, y) {
             character.stopAnimation();
 
             if (Lib().hasInputKey(39)) {
-                character.setSpriteX(0);
-                character.setSpriteY(100);
+                character.setSpriteX(712 * 0);
+                character.setSpriteY(712 * 2);
+                character.setFrames([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
                 character.reverseAnimation(false);
                 character.increaseX();
                 character.resumeAnimation();
             }
             if (Lib().hasInputKey(37)) {
-                character.setSpriteX(0);
-                character.setSpriteY(0);
+                character.setSpriteX(712 * 0);
+                character.setSpriteY(712 * 3);
+                character.setFrames([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
                 character.reverseAnimation(true);
                 character.decreaseX();
                 character.resumeAnimation();
             }
             if (Lib().hasInputKey(40)) {
-                character.setSpriteX(0);
-                character.setSpriteY(200);
+                character.setSpriteX(712 * 0);
+                character.setSpriteY(712 * 0);
+                character.setFrames([0, 1, 2, 3, 4, 5, 6]);
                 character.reverseAnimation(false);
                 character.increaseY();
                 character.resumeAnimation();
             }
             if (Lib().hasInputKey(38)) {
-                character.setSpriteX(0);
-                character.setSpriteY(300);
+                character.setSpriteX(712 * 0);
+                character.setSpriteY(712 * 1);
+                character.setFrames([0, 1, 2, 3, 4]);
                 character.reverseAnimation(false);
                 character.decreaseY();
                 character.resumeAnimation();
             }
 
             var tileList = [[0, "speed", 70], [1, "speed", 40], [2, "stop"], [3, "keyInteract", "treasureTile", 32, chestHandle]];
-            checkCollisionsBase(worldArray, character, 102, 121, tileList);
+            checkCollisionsBase(worldArray, character, 200, 250, tileList);
         });
     });
 }
