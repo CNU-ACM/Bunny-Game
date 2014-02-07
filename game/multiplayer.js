12b561307a215f8543518c7f86301130bf0c6d15
juanvallejo <juan.vallejo.12@cnu.edu>
2014-02-07 09:03:39 -0500

    fixed server address change

--- game/multiplayer.js
+++ game/multiplayer.js
@@ -1,4 +1,4 @@
-var server = 'http://10.67.31.165:8888';/*'http://192.168.1.125:8888';*/
+var server = 'http://10.67.29.243:8888';
 var iojs = 'https://mmo-greenteam.rhcloud.com:8443/socket.io/socket.io.js';
 if(window.location.hostname == 'mmo-greenteam.rhcloud.com') server = 'https://mmo-greenteam.rhcloud.com:8443/';
 else if(window.location.hostname == 'localhost') server = 'http://localhost:8888';
